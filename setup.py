087d9d7f74711c012c4ed91cdc142752819eb896
KayLa <35053868+kaylathomas@users.noreply.github.com>
2023-11-17 22:08:54 -0500

    Initial commit.

---
+++ setup.py
@@ -0,0 +1,28 @@
+from distutils.core import setup
+
+setup(
+  name='FilesNFolders',
+  packages=[],
+  version = '0.1',
+  license='MIT',
+  description = 'Nullam nulla eros, ultricies sit amet, nonummy id, imperdiet feugiat, pede.',
+  author = 'Your Name',
+  author_email = 'yourname@email.com',
+  url = 'https://github.com/yourname/',
+  download_url = 'https://github.com/kaylathomas/RailsStringMethods/archive/refs/tags/v_0.2.tar.gz',
+  keywords = ['some', 'relevant', 'keywords'],
+  install_requires=[
+      'package',
+      'another package'
+  ],
+  classifiers=[
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+  ],
+)
